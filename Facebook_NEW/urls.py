e289b48ad56519a18fd87c6edf6bc521ec5dd938
Ronan Pierce Higgins <ronan.higgins@ucdconnect.ie>
2015-12-19 11:54:36 +0000

    Update urls.py

--- Facebook_NEW/urls.py
+++ Facebook_NEW/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from .settings import MEDIA_ROOT
 from accounts import views
+import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
